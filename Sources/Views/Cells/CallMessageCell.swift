0139bf3da9feac6b5bfd862a7d62ccac2f8a2d87
Vu Dinh Din <vudinhdin@gapo.com.vn>
2020-11-30 14:13:53 +0700

    update call cell

--- Sources/Views/Cells/CallMessageCell.swift
+++ Sources/Views/Cells/CallMessageCell.swift
@@ -9,20 +9,17 @@ import UIKit
 
 open class CallMessageCell: MessageContentCell {
         
-    var callBackTappCell: (()->Void)?
     private let boundView: UIView = {
         let view = UIView()
         view.backgroundColor = UIColor.groupTableViewBackground
         view.layer.cornerRadius = 14
         view.clipsToBounds = true
-        view.translatesAutoresizingMaskIntoConstraints = false
         return view
     }()
     
     let imageView: UIImageView = {
         let img = UIImageView()
         img.image = UIImage(named: "callaudio_answer")
-        img.translatesAutoresizingMaskIntoConstraints = false
         return img
     }()
     let typeCallLabel: UILabel = {
@@ -30,9 +27,9 @@ open class CallMessageCell: MessageContentCell {
         label.text = "Cuộc gọi"
         label.textAlignment = .left
         label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
     private let descriptionLabel: UILabel = {
         let label = UILabel()
         label.text = "Gọi lại"
@@ -41,74 +38,22 @@ open class CallMessageCell: MessageContentCell {
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
-    lazy var callButton: UIButton = {[unowned self] in
-        let button = UIButton(type: .system)
-        button.addTarget(self, action: #selector(makeACall), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.isUserInteractionEnabled = true
-        button.isEnabled = true
-        return button
-    }()
-    @objc func makeACall(sender: UIButton){
-        print("tappppppp")
-        self.callBackTappCell?()
-    }
     
     open override func setupSubviews() {
         super.setupSubviews()
         self.isUserInteractionEnabled = true
         self.messageContainerView.addSubview(boundView)
-        boundView.fillSuperview()
-        self.addImageView()
-        self.addTypeCallLabel()
-        self.addTextLabel()
-//        self.addCallButton()
-    }
-    
-    private func addImageView(){
         boundView.addSubview(imageView)
-        let contentInset = MKMessageConstant.ContentInsets.Call.statusIcon
-        NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: self.boundView.leadingAnchor, constant: contentInset.left),
-            imageView.centerYAnchor.constraint(equalTo: self.boundView.centerYAnchor),
-            imageView.widthAnchor.constraint(equalToConstant: MKMessageConstant.Sizes.Call.statusIcon.width),
-            imageView.heightAnchor.constraint(equalToConstant: MKMessageConstant.Sizes.Call.statusIcon.width)
-        ])
-    }
-    private func addTypeCallLabel(){
         boundView.addSubview(typeCallLabel)
-        let contentInset = MKMessageConstant.ContentInsets.Call.callInfo
-        NSLayoutConstraint.activate([
-            typeCallLabel.leadingAnchor.constraint(equalTo: self.imageView.trailingAnchor, constant: contentInset.left),
-            typeCallLabel.topAnchor.constraint(equalTo: self.imageView.topAnchor, constant: 1),
-            typeCallLabel.heightAnchor.constraint(equalToConstant: 19),
-            typeCallLabel.trailingAnchor.constraint(equalTo: self.boundView.trailingAnchor, constant: -contentInset.left)
-        ])
-    }
-    private func addTextLabel(){
         boundView.addSubview(descriptionLabel)
-        NSLayoutConstraint.activate([
-            descriptionLabel.leadingAnchor.constraint(equalTo: self.typeCallLabel.leadingAnchor),
-            descriptionLabel.bottomAnchor.constraint(equalTo: self.imageView.bottomAnchor, constant: -1),
-            descriptionLabel.heightAnchor.constraint(equalToConstant: 15),
-            descriptionLabel.trailingAnchor.constraint(equalTo: self.typeCallLabel.trailingAnchor)
-        ])
-    }
-    private func addCallButton(){
-        messageContainerView.addSubview(callButton)
-        callButton.fillSuperview()
     }
     
     open override func configure(with message: MKMessageType, at indexPath: IndexPath, and messagesCollectionView: MessagesCollectionView) {
         super.configure(with: message, at: indexPath, and: messagesCollectionView)
-        guard let dataSource = messagesCollectionView.messagesDataSource else {
-            fatalError(MessageKitError.nilMessagesDataSource)
-        }
         self.messageContainerView.backgroundColor = .clear
         switch message.kind {
         case .call(let image, let statusInfo, let callInfo):
             self.updateUI(image: image, statusInfo: statusInfo, callInfo: callInfo)
-            
         default:
             break
         }
@@ -119,7 +64,17 @@ open class CallMessageCell: MessageContentCell {
         self.imageView.image = image
         self.typeCallLabel.text = statusInfo
         self.descriptionLabel.text = callInfo
+        boundView.frame = self.messageContainerView.bounds
+        let contentInset = MKMessageConstant.ContentInsets.Call.statusIcon
         
-        
+        let imageOriginX: CGFloat = contentInset.left
+        let imageOriginY: CGFloat = boundView.frame.midY - MKMessageConstant.Sizes.Call.statusIcon.height/2
+        imageView.frame = CGRect(imageOriginX, imageOriginY, MKMessageConstant.Sizes.Call.statusIcon.width, MKMessageConstant.Sizes.Call.statusIcon.height)
+
+        let callInfoInset = MKMessageConstant.ContentInsets.Call.callInfo
+        let typeCallOriginX: CGFloat = imageView.frame.maxX + callInfoInset.left
+        typeCallLabel.frame = CGRect(typeCallOriginX, imageOriginY + 1, self.boundView.bounds.width - imageView.frame.maxX - callInfoInset.horizontal, 19)
+
+        descriptionLabel.frame = CGRect(typeCallOriginX, imageView.frame.maxY - 15 - 1, typeCallLabel.frame.width, 15)
     }
 }
